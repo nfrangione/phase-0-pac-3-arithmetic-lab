8e42b3f0cfcee888f534c322a028dd46f5a7cefd
Nicholas V Frangione <nfrangione8@gmail.com>
2021-02-18 13:36:56 -0800

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+
+function add (a, b) {
+    var sum = a + b
+    return sum
+}
+function subtract (a, b) {
+    var remainder = a - b
+    return remainder
+}
+function multiply (a, b) {
+    var product = a * b
+    return product
+}
+function divide (a, b) {
+    var quotient = a / b
+    return quotient
+}
+function increment (a) {
+    a++
+    return a
+}
+function decrement (a) {
+    a--
+    return a
+}
+function makeInt(n) {
+    return parseInt(n, 10)
+  }
+function preserveDecimal(string) {
+    return parseFloat(string)
+}
